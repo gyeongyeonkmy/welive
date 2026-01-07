06d74194d37877b8a18875b50d2aaefb4b1091cb
HS-hand <shs0714@khu.ac.kr>
2026-01-07 15:37:45 +0900

    [fix] 충돌 해결

--- src/application/ports/repos/query/i-notice-query-repo.ts
+++ src/application/ports/repos/query/i-notice-query-repo.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NoticeCategory } from '@prisma/client';
 import { NoticeView } from '../../../query/views/notice-view';
 
@@ -11,17 +10,3 @@ export interface INoticeQueryRepo {
     category: NoticeCategory,
   ): Promise<NoticeView[]>;
 }
-=======
-import { NoticeCategory } from '@prisma/client';
-import { NoticeView } from '../../../query/views/notice-view';
-
-export interface INoticeQueryRepo {
-  findById(noticeId: string): Promise<NoticeView>;
-  findAll(
-    page: number,
-    limit: number,
-    searchKeyword: string,
-    category: NoticeCategory,
-  ): Promise<NoticeView[]>;
-}
->>>>>>> cef6f2a ([fix] schema에 options 테이블 추가)
--- src/application/ports/repos/query/i-poll-query-repo.ts
+++ src/application/ports/repos/query/i-poll-query-repo.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { PollStatus } from '@prisma/client';
 import { PollView } from '../../../query/views/poll-view';
 
@@ -12,18 +11,3 @@ export interface IPollQueryRepo {
     building: number,
   ): Promise<PollView[]>;
 }
-=======
-import { PollStatus } from '@prisma/client';
-import { PollView } from '../../../query/views/poll-view';
-
-export interface IPollQueryRepo {
-  findById(pollId: string): Promise<PollView>;
-  findAll(
-    page: number,
-    limit: number,
-    searchKeyword: string,
-    status: PollStatus,
-    building: number,
-  ): Promise<PollView[]>;
-}
->>>>>>> cef6f2a ([fix] schema에 options 테이블 추가)
