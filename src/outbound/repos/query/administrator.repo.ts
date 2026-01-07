ebf0f772227db768d6c28883dce932d4d3051dca
juho-creator <juhokim2018@gmail.com>
2026-01-07 18:54:50 +0900

    feat: schema 수정

---
+++ src/outbound/repos/query/administrator.repo.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from "@prisma/client";
+import { IAdministratorQueryRepo } from "../../../application/ports/repos/I.administrator.query.repo";
+
+export const createAdministratorQueryRepo = (prisma: PrismaClient): IAdministratorQueryRepo => {
+    const findAll = (
+        page: number,
+        limit: number,
+        searchKeyword: string,
+        joinStatus: string
+    ) => {
+        const skip = (page - 1) * limit;
+
+        return prisma.users.findMany({
+            where: {
+                role: "USER",
+                ...(searchKeyword && {
+                    OR: [
+                        { username: { contains: searchKeyword } },
+                        { email: { contains: searchKeyword } }
+                    ]
+                })
+            },
+            include: {
+                aparment: true
+            },
+            skip: skip,
+            take: limit
+        })
+    }
+    return {
+        findAll
+    }
+}
