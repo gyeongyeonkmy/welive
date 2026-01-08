4340bad5114eeef3c0ea4dc094347e43fc094bf1
jung-insung <yon9170@naver.com>
2026-01-08 15:32:42 +0900

    [fix] resident entity

---
+++ src/application/command/entities/user/base-entity.ts
@@ -0,0 +1,7 @@
+export type BaseUserFields = {
+  id: string
+  d
+  
+  createdAt: Date
+  updatedAt: Date
+}
---
+++ src/application/command/entities/user/resident-entity.ts
@@ -0,0 +1,10 @@
+export type ResidentEntity {
+  
+}
+
+export const ResidentEntity = {
+  private _building: number;
+  private _unit: number;
+  private _isHouseholder: boolean;
+  
+}
