835ff2fad274bf4c23f0873be740978c4a173f24
gyeongyeonk <zkkzy98@gmail.com>
2026-02-03 10:47:10 +0900

    [fix]빼먹은파일..

--- src/domain/notification/interface/i-notification-command.ts
+++ src/domain/notification/interface/i-notification-command.ts
@@ -3,4 +3,5 @@ import { NotificationProps } from '../entity/notification';
 export interface INotificationCommandRepo {
   markAsRead(notificationId: string): Promise<void>;
   bulkSave(bulk: NotificationProps[]): Promise<void>;
+  delete(): Promise<void>;
 }
